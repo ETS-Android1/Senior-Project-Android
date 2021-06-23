12176a4aa29f3b5e4e3db7cc506f6b5deaa92e2d
Alex Brito <alexbrito@Alexs-MacBook-Pro.local>
2021-06-22 21:51:01 -0400

    First Layout

--- app/src/main/java/com/example/seniorproject/MainActivity.java
+++ app/src/main/java/com/example/seniorproject/MainActivity.java
@@ -3,36 +3,31 @@ package com.example.seniorproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.seniorproject.Authentication.Authentication;
-import com.example.seniorproject.User.User;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private Button logInButton;
+    private ImageView homePage;
     private Button signUpButton;
-    private EditText userNameEditText;
-    private EditText passwordEditText;
-    private Authentication authentication;
-
-
-
+    private Button logInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logInButton = (Button) findViewById(R.id.logIn_Button);
-        userNameEditText = (EditText) findViewById(R.id.userName_TextBox);
-        passwordEditText = (EditText) findViewById(R.id.userPassword_Textfield);
+        homePage = (ImageView) findViewById(R.id.homepage_imageview);
 
-        logInButton.setOnClickListener(e -> {
-            System.out.println("Hello");
+        signUpButton = (Button) findViewById(R.id.signup_button);
+        logInButton = (Button) findViewById(R.id.login_button);
+        signUpButton.setOnClickListener(e -> {
+            System.out.println("Kam Clicked Me");
         });
     }
 }
